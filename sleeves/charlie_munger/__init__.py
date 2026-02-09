adeca645f60f358fdca03c576b3bb44e5edbf6dc
BigO2244 <brett.olson@gmail.com>
2026-02-09 11:30:38 -0500

    Add charlie_munger sleeve package and execution-email semantics

---
+++ sleeves/charlie_munger/__init__.py
@@ -0,0 +1,9 @@
+from .backtest import run_backtest_with_details
+from .config import CharlieMungerConfig, load_config
+
+__all__ = [
+    "CharlieMungerConfig",
+    "load_config",
+    "run_backtest_with_details",
+]
+
